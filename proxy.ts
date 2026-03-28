dc20ec403f3212e7ed0d329260b90a8e5a0b0853
habdullahjaved <habdullahjaved560@gmail.com>
2026-03-28 06:37:36 +0400

    added readme

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,45 @@
+// // proxy.ts (root)
+// import { NextRequest, NextResponse } from "next/server";
+// import { updateSession } from "@/lib/supabase/middleware";
+// import { getToken } from "next-auth/jwt";
+
+// const PROTECTED_ROUTES = ["/dashboard"];
+
+// export default async function proxy(request: NextRequest) {
+//   // ← default added
+//   const { pathname } = request.nextUrl;
+
+//   const response = await updateSession(request);
+
+//   const token = await getToken({
+//     req: request,
+//     secret: process.env.AUTH_SECRET,
+//   });
+
+//   if (PROTECTED_ROUTES.some((route) => pathname.startsWith(route))) {
+//     if (!token) {
+//       const loginUrl = new URL("/login", request.url);
+//       loginUrl.searchParams.set("callbackUrl", pathname);
+//       return NextResponse.redirect(loginUrl);
+//     }
+//   }
+
+//   return response;
+// }
+
+// export const config = {
+//   matcher: [
+//     "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|gif|webp)$).*)",
+//   ],
+// };
 // proxy.ts (root)
 import { NextRequest, NextResponse } from "next/server";
-import { updateSession } from "@/lib/supabase/middleware";
 import { getToken } from "next-auth/jwt";
 
 const PROTECTED_ROUTES = ["/dashboard"];
 
 export default async function proxy(request: NextRequest) {
-  // ← default added
   const { pathname } = request.nextUrl;
-
-  const response = await updateSession(request);
 
   const token = await getToken({
     req: request,
@@ -24,7 +54,7 @@ export default async function proxy(request: NextRequest) {
     }
   }
 
-  return response;
+  return NextResponse.next();
 }
 
 export const config = {
